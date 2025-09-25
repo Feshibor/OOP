2f4cf0c06b783480a62b1eb2d3ba81f49a27cd05
Feshibor <123161809+Feshibor@users.noreply.github.com>
2025-09-25 18:15:32 +0300

    Update test_sample.cpp

--- test/test_sample.cpp
+++ test/test_sample.cpp
@@ -3,7 +3,30 @@
 #include <sstream>
 #include <iostream>
 
-TEST(VowelCounterTest, EmptyString) {
-    EXPECT_EQ(94,"doksan dört");
+void runTest(int input, const std::string& expected) {
+    std::istringstream in(std::to_string(input));
+    std::ostringstream out;
+
+    std::streambuf* cinBackup = std::cin.rdbuf(in.rdbuf());
+    std::streambuf* coutBackup = std::cout.rdbuf(out.rdbuf());
+
+    intToTurkish(); 
+
+    std::cin.rdbuf(cinBackup);
+    std::cout.rdbuf(coutBackup);
+
+    std::string result = out.str();
+    if (!result.empty() && result.back() == '\n') result.pop_back(); 
+
+    EXPECT_EQ(result, expected);
 }
 
+TEST(IntToStrTest, TurkishNumbers) {
+    runTest(1,"bir");
+    runTest(13,"on üç");
+    runTest(27,"yirmi yedi");
+    runTest(38,"otuz sekiz");
+    runTest(40,"kırk");
+    runTest(77,"yetmiş yedi");
+    runTest(94,"doksan dört");
+}
