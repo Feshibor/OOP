ae71dc5f5aa7f5c0f71b90cf201f67c47e93dcce
Feshibor <123161809+Feshibor@users.noreply.github.com>
2025-09-25 13:27:33 +0300

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -2,56 +2,58 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int intToTurkish() {
     int x;
-    std::cin >> x;
+    cin >> x;
     if (x == 0) {
-        std::cout <<"sıfır";
+        cout <<"sıfır";
         return 0;
     }
     int y = x / 10;
     x = x % 10;
     if (y == 1) {
-        std::cout <<"on";
+        cout <<"on";
     } else if (y == 2) {
-        std::cout <<"yirmi";
+        cout <<"yirmi";
     } else if (y == 3) {
-        std::cout <<"otuz";
+        cout <<"otuz";
     } else if (y == 4) {
-        std::cout <<"kırk";
+        cout <<"kırk";
     } else if (y == 5) {
-        std::cout <<"elli";
+        cout <<"elli";
     } else if (y == 6) {
-        std::cout <<"altmış";
+        cout <<"altmış";
     } else if (y == 7) {
-        std::cout <<"yetmiş";
+        cout <<"yetmiş";
     } else if (y == 8) {
-        std::cout <<"seksen";
+        cout <<"seksen";
     } else if (y == 9) {
-        std::cout <<"doksan";
+        cout <<"doksan";
     } 
     if (x!=0 && y!=0){
-        std::cout <<" ";
+        cout <<" ";
     }
 
     if (x == 1) {
-        std::cout <<"bir";
+        cout <<"bir";
     } else if (x == 2) {
-        std::cout <<"iki";
+        cout <<"iki";
     } else if (x == 3) {
-        std::cout <<"üç";
+        cout <<"üç";
     } else if (x == 4) {
-        std::cout <<"dört";
+        cout <<"dört";
     } else if (x == 5) {
-        std::cout <<"beş";
+        cout <<"beş";
     } else if (x == 6) {
-        std::cout <<"altı";
+        cout <<"altı";
     } else if (x == 7) {
-        std::cout <<"yedi";
+        cout <<"yedi";
     } else if (x == 8) {
-        std::cout <<"sekiz";
+        cout <<"sekiz";
     } else if (x == 9) {
-        std::cout <<"dokuz";
+        cout <<"dokuz";
     } 
     return 0;
 }
